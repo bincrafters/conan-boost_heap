06e8fd9f9e5164d3daf169699448437131e6b15b
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:23:20 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BoostHeapConan(ConanFile):
     version = "1.64.0"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-heap"
-    source_url = "https://github.com/boostorg/heap"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["heap"]
@@ -25,9 +24,12 @@ class BoostHeapConan(ConanFile):
                       #array3 assert1 bind3 concept_check5 config0 intrusive6 iterator5 mpl5 parameter10 static_assert1 throw_exception2 type_traits3
                       
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def package(self):
         for lib_short_name in self.lib_short_names:
