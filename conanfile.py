445d502486ff88a65b4d359eedcba103ca77dbb3
Jerry W <jerrywiltse@gmail.com>
2018-02-02 07:08:26 -0500

    Merge testing into stable

--- conanfile.py
+++ conanfile.py
@@ -17,19 +17,19 @@ class BoostHeapConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_array/1.66.0@bincrafters/testing",
-        "boost_assert/1.66.0@bincrafters/testing",
-        "boost_bind/1.66.0@bincrafters/testing",
-        "boost_concept_check/1.66.0@bincrafters/testing",
-        "boost_config/1.66.0@bincrafters/testing",
-        "boost_intrusive/1.66.0@bincrafters/testing",
-        "boost_iterator/1.66.0@bincrafters/testing",
-        "boost_mpl/1.66.0@bincrafters/testing",
-        "boost_parameter/1.66.0@bincrafters/testing",
-        "boost_static_assert/1.66.0@bincrafters/testing",
-        "boost_throw_exception/1.66.0@bincrafters/testing",
-        "boost_type_traits/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.66.0@bincrafters/stable",
+        "boost_array/1.66.0@bincrafters/stable",
+        "boost_assert/1.66.0@bincrafters/stable",
+        "boost_bind/1.66.0@bincrafters/stable",
+        "boost_concept_check/1.66.0@bincrafters/stable",
+        "boost_config/1.66.0@bincrafters/stable",
+        "boost_intrusive/1.66.0@bincrafters/stable",
+        "boost_iterator/1.66.0@bincrafters/stable",
+        "boost_mpl/1.66.0@bincrafters/stable",
+        "boost_parameter/1.66.0@bincrafters/stable",
+        "boost_static_assert/1.66.0@bincrafters/stable",
+        "boost_throw_exception/1.66.0@bincrafters/stable",
+        "boost_type_traits/1.66.0@bincrafters/stable"
     )
 
     # BEGIN
@@ -37,7 +37,7 @@ class BoostHeapConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.66.0@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
