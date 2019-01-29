e5ff9d6ab75d1dbb46ad3349e6e9077118c65577
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:13 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostHeapConan(base.BoostBaseConan):
     name = "boost_heap"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_heap"
     lib_short_names = ["heap"]
     header_only_libs = ["heap"]
